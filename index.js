f357af816a1990cfd9826f8af07dbf9c1cd9ff69
Miguel Gonzalez <mgonzalez@did.grupodiusframi.com>
2016-07-18 09:19:25 +0200

    Add Push ClientKey

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
+  }/*,
   push: {
     android: {
         senderId: '136537246544',
@@ -31,7 +31,7 @@ var api = new ParseServer({
         bundleId: '',
         production: true
     }
-  }
+  }*/
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
