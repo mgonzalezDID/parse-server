4c66c9934ae727fc9b1c11b8791c3e17532f7847
Miguel Gonzalez <mgonzalez@did.grupodiusframi.com>
2016-07-20 14:21:48 +0200

    Pushy 1.0.6

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var path = require('path');
 var Pushy = require('pushy-node');
 var PushyPushAdapter = require('pushy-adapter');
 var pushyAPI = new Pushy('be4e26586f14abbe52c759b175869d7022bbf4adacd5914e006bb2a50d4ed569');
-/*var pushyPushAdapter = new PushyPushAdapter({
+var pushyPushAdapter = new PushyPushAdapter({
   pushyApiKey:"be4e26586f14abbe52c759b175869d7022bbf4adacd5914e006bb2a50d4ed569"
-});*/
+});
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -28,10 +28,7 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
   push: {
-    android: {
-        senderId: '136537246544',
-        apiKey: 'AIzaSyA-c1B76BmFx--1Bk7qfCD10_t1jgwc09o'
-    }
+    adapter: pushyPushAdapter
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
