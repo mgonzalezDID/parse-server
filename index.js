a74b4d5a15c2c4a01ac7f6ec30110412c036660e
Miguel Gonzalez <mgonzalez@did.grupodiusframi.com>
2016-07-20 12:21:48 +0200

    Added PushyPushAdapter

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 var Pushy = require('pushy-node');
 var pushyAPI = new Pushy('be4e26586f14abbe52c759b175869d7022bbf4adacd5914e006bb2a50d4ed569');
-var PushyPushAdapter = require('PushyPushAdapter');
+//var PushyPushAdapter = require('PushyPushAdapter');
 /*var pushyPushAdapter = new PushyPushAdapter({
   pushyApiKey:"be4e26586f14abbe52c759b175869d7022bbf4adacd5914e006bb2a50d4ed569"
 });*/
